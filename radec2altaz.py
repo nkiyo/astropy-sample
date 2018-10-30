3235b44682d76edf8da848b6365176b22d0d8e6c
nkiyo <nkiyo@hoge.com>
2018-10-30 09:14:20 +0900

    init commit

---
+++ radec2altaz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+from astropy import units as u
+from astropy.coordinates import SkyCoord, EarthLocation
+
+observation_location = EarthLocation(lat='52.2532', lon='351.639', height=100*u.m)
+observation_time = Time('2018-10-30 00:30:59')
+aa = AltAz(location=observation_location, obstime=observation_time)
+sc = SkyCoord(ra=10.625*u.degree, dec=41.2*u.degree, frame='icrs')
+sc.transform_to(aa)
+
